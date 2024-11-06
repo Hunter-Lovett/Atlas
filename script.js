b862624203c640133221bacb16c5dd2f0d8de00f
Hunter-Lovett <huntellovet26@waupacaschools.org>
2024-11-05 18:54:19 -0600

    adding speach recconition

---
+++ script.js
@@ -0,0 +1,14 @@
+function listen() {
+    let inputArea = document.getElementById('input-area')
+    let outputArea = document.getElementById('output-area')
+  
+    var recognition = new webkitSpeechRecognition();
+    recognition.lang = "en-GB";
+    recognition.start();
+  
+    recognition.onresult = function(event) {
+      let transcript = event.results[0][0].transcript;
+      inputArea.innerHTML = event.results[0][0].transcript;
+    }
+  }
+  
